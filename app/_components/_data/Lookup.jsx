161f68b4ab09b7a70d196d84ff65fd7a22e9c3ff
Krunal Gupta <guptakrunal02@gmail.com>
2025-01-07 16:55:39 +0530

    Init Header Page

---
+++ app/_components/_data/Lookup.jsx
@@ -0,0 +1,6 @@
+export default {
+  HeroHeading: "AI Logo Maker",
+  HeroSubheading: "Perfect Logos for Apps, Businesses and Websites",
+  HeroDesc: "Craft unique and professional effortlessly with AI Logo Maker.",
+  InputTitlePlaceholder: "Enter your Logo name",
+};
