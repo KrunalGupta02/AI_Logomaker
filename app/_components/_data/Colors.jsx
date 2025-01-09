cd7bbe27bc3a95a98007edcccb5456655d41f8dc
Krunal Gupta <guptakrunal02@gmail.com>
2025-01-09 17:15:35 +0530

    Init the palette section

---
+++ app/_components/_data/Colors.jsx
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "Let Us Select",
+    colors: ["#ff5733", "#33ff57", "#3357ff", "#ff33a8", "#f4ff33"],
+  },
+  {
+    name: "Ocean Blues",
+    colors: ["#003f5c", "#2f4b7c", "#665191", "#a05195", "#d45087"],
+  },
+  {
+    name: "Sunset Warmth",
+    colors: ["#ff7e67", "#ff9770", "#ffb07b", "#ffc385", "#ffdb90"],
+  },
+  {
+    name: "Earthy Browns",
+    colors: ["#8d5524", "#c68642", "#e0ac69", "#f1c27d", "#ffdbac"],
+  },
+  {
+    name: "Shades of Purple",
+    colors: ["#E6E6FA", "#D8BFD8", "#DDA0DD", "#EE82EE", "#DA70D6"],
+  },
+  {
+    name: "Shades of Red",
+    colors: ["#FF0000", "#DC143C", "#B22222", "#8B0000", "#FF6347"],
+  },
+];
--- app/create/_components/LogoPallete.jsx
+++ app/create/_components/LogoPallete.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import HeadingDescription from "./HeadingDescription";
+import Lookup from "@/app/_components/_data/Lookup";
+import Colors from "@/app/_components/_data/Colors";
 
-const LogoPallete = () => {
-  return <div>LogoPallete</div>;
+const LogoPallete = ({ onHandleInputChange }) => {
+  const [selectedOption, setSelectedOption] = useState();
+  return (
+    <div className="my-10">
+      <HeadingDescription
+        title={Lookup.LogoColorPaletteTitle}
+        description={Lookup.LogoColorPaletteDesc}
+      />
+
+      {/* Color Palette */}
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-5">
+        {Colors.map((palette, index) => (
+          <div
+            className={`flex p-1 cursor-pointer
+            ${
+              selectedOption == palette?.name &&
+              "border-2 rounded-lg border-primary"
+            }`}
+            key={index}
+          >
+            {palette?.colors.map((color, index) => (
+              <div
+                className="h-24 w-full"
+                key={index}
+                style={{ backgroundColor: color }}
+                onClick={() => {
+                  setSelectedOption(palette.name);
+                  onHandleInputChange(palette.name);
+                }}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LogoPallete;
