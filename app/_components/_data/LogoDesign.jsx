ea9010a4456565c3c3d1577ee8445b66a74500d1
Krunal Gupta <guptakrunal02@gmail.com>
2025-01-09 17:49:32 +0530

    Init the logo design images section

---
+++ app/_components/_data/LogoDesign.jsx
@@ -0,0 +1,35 @@
+export default [
+  {
+    title: "Cartoon Logo",
+    image: "/design_1.png",
+    prompt:
+      "Create a set of vibrant, playful logo design featuring the famous Cartoon",
+  },
+  {
+    title: "App Logo",
+    image: "/design_2.png",
+    prompt: "Create a vibrant and playful 3D Logo for the app.",
+  },
+  {
+    title: "Modern Mascot Logos",
+    image: "/design_3.png",
+    prompt: "A vibrant logo featuring a friendly , animated character",
+  },
+  {
+    title: "Black and White Logos",
+    image: "/design_4.png",
+    prompt: "Create a set of black and white logo designs for a new brand",
+  },
+  {
+    title: "Miniliaistic and Elegant Logo",
+    image: "/design_5.png",
+    prompt:
+      "Create a sophisticated and Elegant logo design that is inspired by the brand name",
+  },
+  {
+    title: "Vintage Custom Logo",
+    image: "/design_6.png",
+    prompt:
+      "Create a vintage logo design, hand drawn that is inspired by the brand name",
+  },
+];
--- app/create/_components/LogoDesigns.jsx
+++ app/create/_components/LogoDesigns.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { Heading } from "lucide-react";
+import React, { useState } from "react";
+import HeadingDescription from "./HeadingDescription";
+import Lookup from "@/app/_components/_data/Lookup";
+import LogoDesign from "@/app/_components/_data/LogoDesign";
+import Image from "next/image";
 
-const LogoDesigns = () => {
-  return <div>LogoDesigns</div>;
+const LogoDesigns = ({ onHandleInputChange, formData }) => {
+  const [selectedOption, setSelectedOption] = useState(formData?.design?.title);
+  return (
+    <div className="my-10">
+      <HeadingDescription
+        title={Lookup.LogoDesignTitle}
+        description={Lookup.LogoDesignTitle}
+      />
+
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-10 mt-10">
+        {LogoDesign.map((design, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              setSelectedOption(design.title);
+              onHandleInputChange(design);
+            }}
+            className={`p-1 hover:border border-2 cursor-pointer ${
+              selectedOption == design.title &&
+              "border-2 border-primary rounded-xl"
+            }`}
+          >
+            <Image
+              className="w-full rounded-xl h-[150px] object-fill"
+              src={design.image}
+              alt={design.title}
+              width={300}
+              height={200}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LogoDesigns;
