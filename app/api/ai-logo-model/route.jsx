3090eb52941963c9d77a5a4f1be86389a7fd6330
Krunal Gupta <guptakrunal02@gmail.com>
2025-02-19 17:58:34 +0530

    Integrate the Hugging Face AI model 'luke'

--- app/api/ai-logo-model/route.jsx
+++ app/api/ai-logo-model/route.jsx
@@ -1,4 +1,5 @@
 import { AILogoPrompt } from "@/configs/AiModel";
+import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -7,10 +8,35 @@ export async function POST(req) {
   try {
     // Generate AI Text Prompt for Logo
     const AIPromptResult = await AILogoPrompt.sendMessage(prompt);
-    console.log(JSON.parse(AIPromptResult.response.text()).prompt);
+    const AIPrompt = AIPromptResult.response.text().prompt;
+    console.log(JSON.parse(AIPrompt));
 
-    return NextResponse.json(JSON.parse(AIPromptResult.response.text()).prompt);
+    // Generate Logo From AI Model
+    const response = await axios.post(
+      "https://router.huggingface.co/hf-inference/models/strangerzonehf/Flux-Midjourney-Mix2-LoRA",
+      AIPrompt,
+      {
+        headers: {
+          Authorization: "Bearer" + process.env.HUGGING_FACE_API_KEY,
+          "Content-Type": "application/json",
+        },
+        responseType: "arraybuffer",
+      }
+    );
+    // Convert to Base64 Image
+    const buffer = Buffer.from(response.data, "binary");
+    const base64Image = buffer.toString("base64");
+
+    const base64ImageWithMime = `data:image/png;base64,${base64Image}`;
+
+    console.log(base64ImageWithMime);
+
+    // Save to Firebase DB
+
+    return NextResponse.json({ image: base64ImageWithMime });
 
     // Pass it to AI Image Model
-  } catch (e) {}
+  } catch (e) {
+    return NextResponse.json({ error: e });
+  }
 }
