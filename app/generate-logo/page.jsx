92bfec1ff5034847b5fae3b3a220ffd0eab8b36e
Krunal Gupta <guptakrunal02@gmail.com>
2025-02-20 18:29:51 +0530

    Add image download functionality and loading spinner to logo generation

--- app/generate-logo/page.jsx
+++ app/generate-logo/page.jsx
@@ -7,6 +7,9 @@ import axios from "axios";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { toast } from "sonner";
+import { RingLoader } from "react-spinners";
+import { Download, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
 
 const GenerateLogo = () => {
   const { userDetail, setUserDetail } = useContext(UserDetailContext);
@@ -45,6 +48,17 @@ const GenerateLogo = () => {
     }
   }, [logoImage]);
 
+  // Image Download Code
+  const handleImgDownload = (imageSrc) => {
+    // Create a temporary anchor element
+    const link = document.createElement("a");
+    link.href = imageSrc;
+    link.download = "AIImage.png"; // File name for download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   const GenerateAILogo = async () => {
     if (modelType != "Free" && userDetail?.credits <= 0) {
       toast("You don't have enough credits to generate the logo");
@@ -79,16 +93,35 @@ const GenerateLogo = () => {
 
   return (
     <div>
-      <div>
-        <h1>{loading && "Loading..."}</h1>
+      <div className="mt-16 flex flex-col items-center justify-center ">
+        <h1>{loading && <RingLoader size={80} />}</h1>
         {!loading && (
           <Image
             src={logoImage ? logoImage : "/design_1.png"}
             alt="logo"
-            width={200}
-            height={200}
+            width={300}
+            height={300}
+            className="rounded-xl"
           />
         )}
+      </div>
+      <div className="flex justify-center items-center mt-20 gap-10">
+        <button
+          onClick={() =>
+            handleImgDownload(logoImage ? logoImage : "/design_1.png")
+          }
+          className="flex items-center gap-2 bg-primary text-white p-2 px-1.5 rounded-lg text-base"
+        >
+          <Download />
+          Download
+        </button>
+        <Link
+          className="flex items-center gap-2 border rounded-lg border-[#e5e7eb] p-2 px-1.5 text-base"
+          href={"/dashboard"}
+        >
+          <LayoutDashboard />
+          Dashboard
+        </Link>
       </div>
     </div>
   );
