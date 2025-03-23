3e303b42709979ac5d791ae3390b8b225e21b9a2
Krunal Gupta <guptakrunal02@gmail.com>
2025-03-24 00:49:01 +0530

    Init Next Theme for Dark Mode

---
+++ app/_components/DarkLightModeBtn.jsx
@@ -0,0 +1,27 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import React from "react";
+
+const DarkLightModeBtn = () => {
+    const { theme, setTheme, resolvedTheme } = useTheme();
+
+    const toggleTheme = () => {
+        if (resolvedTheme === "light") setTheme("dark");
+        if (resolvedTheme === "dark") setTheme("light");
+    };
+    return (
+        <div className="flex gap-2 items-center">
+            <button type="button" onClick={toggleTheme}>
+                {theme == "dark" ? (
+                    <Moon onClick={toggleTheme} className="text-2xl animate-fade-in-right text-yellow-400 hover:text-purple cursor-pointer"
+                    />
+                ) : (
+                    <Sun onClick={toggleTheme} className="text-2xl animate-fade-in-right text-sky-400 cursor-pointer"
+                    />
+                )}
+            </button>
+        </div>
+    );
+};
+
+export default DarkLightModeBtn;
